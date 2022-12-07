bb616a61daa3ab939ac33db289de8f679a88ec52
curious <vishal238128@gmail.com>
2022-12-07 19:28:18 +0530

    code added in nftmarketplace file

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -7,3 +7,37 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 import "hardhat/console.sol";
 
+contract NFTMarketplace is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _itemsSold;
+
+    uint256 listingPrice = 0.025 ether;
+    address payable owner;
+
+    mapping(uint256 => MarketItem) private idToMarketItem;
+
+    struct MarketItem {
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold;
+    }
+
+    // event can represent current state on blockchain
+    // event can cause, arguments to store in transaction log of blockchain
+
+    event MarketItemCreated (
+        uint256 indexed tokenId,
+        address payable seller,
+        address payable owner,
+        uint256 price,
+        bool sold
+    );
+
+    constructor() ERC721("Metaverse Token", "MET"){
+        owner = payable(msg.sender);
+    }
+
+}
