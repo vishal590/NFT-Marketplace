800abae83a155d8a8f5876cb4abbcc7e60fdb5c6
curious <vishal238128@gmail.com>
2022-12-09 00:37:10 +0530

    code added in sol file

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -98,4 +98,21 @@ contract NFTMarketplace is ERC721URIStorage {
         _transfer(msg.sender, address(this), tokenId);   
     }
 
+    function createMarketSale(
+        uint256 tokenId
+    ) public payable {
+        uint256 price = idToMarketItem[tokenId].price;
+        address seller = idToMarketItem[tokenId].seller;
+        require(msg.value == price, "Please submit the asking price in order to complete the purchase");
+        idToMarketItem[tokenId].owner = payable(msg.sender);
+        idToMarketItem[tokenId].sold = true;
+        idToMarketItem[tokenId].seller = payable(address(0));
+        _itemsSold.increment();
+        _transfer(address(this), msg.sender, tokenId);
+        payable(owner).transfer(listingPrice);
+        payable(seller).transfer(msg.value);
+    }
+
+    // require is error handling global function
+    // msg.value is contracts caller
 }
